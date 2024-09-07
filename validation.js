a8effa2c446580fc437d13e30570e56c3a069fd0
Sadia-js <sjsthi610@gmail.com>
2024-09-07 20:27:46 +0100

    feat: about validation of a function

---
+++ validation.js
@@ -0,0 +1,52 @@
+function multiply(num1, num2){
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return 'Please provide a number';
+    }
+    const mult = num1 * num2;
+    return mult;
+}
+// const result = multiply(5, 'seven');
+// const result = multiply([7, 6], 'seven');
+// const result = multiply(3 + 6); 
+// const result = multiply(3 * 6, 8 + 1);
+
+
+const result = multiply('3', 'seven');
+// console.log(result);
+
+//
+function fullName(first, last){
+    if(typeof first !== 'string'){
+        return 'First name should be a string';
+    }
+    else if(typeof last !== 'string'){
+        return 'Last name should be a string';
+    }
+    const full = first + ' ' + last;
+    return full;
+}
+
+const full = fullName('Sadia', 10);
+// console.log(full);
+
+// Object
+function getPrice(product){
+    if(typeof product !== 'object'){
+        return 'Please provide an object';
+    }
+    const price = product.price;
+    return price;
+}
+
+const price = getPrice({name: 'Kodom', price: 35, color: 'blue'});
+// console.log(price);
+
+function getSecond(numbers){
+    if(Array.isArray(numbers) !== 'array'){
+        return 'Please provide a valid array';
+    }
+    const second = numbers[1];
+    return second;
+}
+const second = getSecond({name: 'sadia', marks: 90});
+console.log(second);
